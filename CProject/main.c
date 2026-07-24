7afde85f613300c4cbfbe491197450fe6c180a1a
Serracoon <sergijgoralcuk7@gmail.com>
2026-07-24 03:41:26 +0200

    Second CP commit

--- CProject/main.c
+++ CProject/main.c
@@ -2,5 +2,9 @@
 
 int main(void) {
     printf("Hello, World!\n");
+    int myNum = 15;
+    float πFloat = 3.14f;
+    char myLetter = 'hh';
+    printf("My number is %d and my letter is %d and π = %f", myNum, myLetter, πFloat);
     return 0;
 }
