5d26c4dcf7256c65edd6dcd0f6a46ce6ee2b12a7
Serracoon <sergijgoralcuk7@gmail.com>
2026-07-30 00:44:49 +0200

    Second Pragram

--- CNeMops/main.c
+++ CNeMops/main.c
@@ -1,6 +1,21 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    uint32_t number = 0x11223344;
+    char *byte_ptr = (char *) &number;
+
+    printf("The number is %x\n", number);
+
+    *byte_ptr = 0xFF;
+
+    printf("Modified 32-bit integer: 0x%X\n", number);
+
+    /*44 стає FF тому що у більшості процесорів працює система "Little-Endian" де багато байтні дані
+     зберігаються послідовно від найновішого до найстарішого і коли програма просить перезаписати данні
+     то першим йде іменно 44
+
+     */
     return 0;
+
 }
